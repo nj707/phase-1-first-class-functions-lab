eedb7b103432bcf11d3958d56970029bec9a75c2
Nial Johnson <nialj707@gmail.com>
2023-07-11 13:06:09 -0700

    first class function lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+//let Drivers = (['Antonia', 'Nuru', 'Amari', 'Mo'])
+
+function returnFirstTwoDrivers (Drivers){
+    return Drivers.slice(0,2)
+}
+function returnLastTwoDrivers(Drivers){
+    return Drivers.slice(2,5)
+}
+let selectingDrivers = ([returnFirstTwoDrivers, returnLastTwoDrivers])
+
+function createFareMultiplier(multiplier){
+    return fare => fare * multiplier 
+}
+
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(arrayOfDrivers, funct){
+    return funct(arrayOfDrivers)
+}
